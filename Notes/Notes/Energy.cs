5f70e4c12a55fb3d60eff6177135ad31a1115c9a
Bradley van Ewijk <32265034+PrimeLockerbee@users.noreply.github.com>
2022-09-19 23:45:16 +0200

    Object Pool
    Added an object pool to the project, with the 1,2 and 3 keys you can spawn the different kind of enemies. For now they can only be active. For the assignments sake I'll add a timer that deactivates them after a while.

---
+++ Notes/Notes/Energy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes
+{
+    class Energy
+    {
+        public static float StatMax = 100;
+
+        public static float EnergyLevel = StatMax;
+    }
+}
---
+++ Notes/Notes/Thirst.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes
+{
+    class Thirst
+    {
+        public static float StatMax = 100;
+
+        public static float ThirstLevel = StatMax;
+
+    }
+}
